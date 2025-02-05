4da06d9edd8fee39362b1ebc7228c122f8cdaab1
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2025-02-05 21:06:39 +0530

    Minor Update

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,24 +9,24 @@ export default function Gallery() {
       <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
         Gallery <br />
       </h1>
-      <div className="flex flex-col w-full max-w-7xl m-5">
+      <div className="flex flex-col w-full max-w-7xl mx-auto p-5">
         <h1 className="text-3xl md:text-4xl font-bold">SNT Pavilion</h1>
         <div className="flex flex-row flex-wrap items-center justify-center m-5">
-          {gallery.PAV.map((image, index) => <Image key={index} src={image} alt="image" height={1200} width={1600} className="h-96 w-[30rem] rounded-lg m-4 cursor-pointer hover:-translate-y-2 transition-transform"/>
+          {gallery.PAV.map((image, index) => <Image key={index} src={image} alt="image" height={1200} width={1600} className="h-96 w-full md:w-[30rem] rounded-lg m-4 cursor-pointer hover:-translate-y-2 transition-transform"/>
         )}
         </div>
       </div>
-      <div className="flex flex-col w-full max-w-7xl m-5">
+      <div className="flex flex-col w-full max-w-7xl mx-auto p-5">
         <h1 className="text-3xl md:text-4xl font-bold">Sessions</h1>
         <div className="flex flex-row flex-wrap items-center justify-center m-5">
-          {gallery.SES.map((image, index) => <Image key={index} src={image} alt="image" height={1200} width={1600} className="h-96 w-[30rem] rounded-lg m-4 cursor-pointer hover:-translate-y-2 transition-transform"/>
+          {gallery.SES.map((image, index) => <Image key={index} src={image} alt="image" height={1200} width={1600} className="h-96 w-full md:w-[30rem] rounded-lg m-4 cursor-pointer hover:-translate-y-2 transition-transform"/>
         )}
         </div>
       </div>
-      <div className="flex flex-col w-full max-w-7xl m-5">
+      <div className="flex flex-col w-full max-w-7xl mx-auto p-5">
         <h1 className="text-3xl md:text-4xl font-bold">Ask And Retrieve</h1>
         <div className="flex flex-row flex-wrap items-center justify-center m-5">
-          {gallery.AR.map((image, index) => <Image key={index} src={image} alt="image" height={1200} width={1600} className="h-96 w-[30rem] rounded-lg m-4 cursor-pointer hover:-translate-y-2 transition-transform"/>
+          {gallery.AR.map((image, index) => <Image key={index} src={image} alt="image" height={1200} width={1600} className="h-96 w-full md:w-[30rem] rounded-lg m-4 cursor-pointer hover:-translate-y-2 transition-transform"/>
         )}
         </div>
       </div>
